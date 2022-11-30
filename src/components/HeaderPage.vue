e83a63d60bb163dce994cfdb8cd1fabc93b09960
MARTINA1102 <martinamiccoli11@gmail.com>
2022-11-30 15:45:45 +0100

    primo commit-impostazione iniziale

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navbar">
+    <h1>BOOLFLIX</h1>
+    <div>
+      <label for="textf">Search your title
+      <input type="text" id="textf" name="textf">
+    </label>
+    <button>CERCA</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPage',
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar{
+  background-color: black;
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  width: 100vw;
+  height: 5rem;
+}
+h1{
+  color: red;
+}
+
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main_page"></div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+};
+</script>
+
+<style lang="scss" scoped>
+.main_page{
+  height: 100vh;
+  background-color: gray;
+  padding-top: 5rem;
+}
+
+</style>
